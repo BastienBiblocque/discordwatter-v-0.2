407529b05a3e4de18b1b5271c68f00a59f203f25
Bastien <45488913+BastienBiblocque@users.noreply.github.com>
2022-01-29 00:16:18 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
+const token = process.env.TOKEN
+var CronJob = require('cron').CronJob;
+
+var job = new CronJob('* * * * * *', async function() {
+    await postMessage('510203975207878687','slt mek bwa 2 lo');
+}, null, true, 'America/Los_Angeles');
+
+client.once('ready', () => {
+    console.log('Félicitations, votre bot Discord a été correctement initialisé !');
+    job.start();
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+        message.reply('pong !')
+    }
+})
+
+
+
+async function postMessage(channelId, message){
+    const channel = await client.channels.fetch(channelId);
+    await channel.send(message);
+}
+
+client.login(token);
+
